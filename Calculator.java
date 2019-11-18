8633544a0a455b578b0c02d89003ce10913c87ac
AmanGupta2003 <57882281+AmanGupta2003@users.noreply.github.com>
2019-11-18 02:05:41 -0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -46,103 +46,111 @@ public class Calculator {
     }
 
     public static String Calculate(String input) {
-        if (input.length() == 0) {
+        if (input.length() == 0) { // this command first checks if the user input is only 1 character long, it will
+                                   // send an error as no calculation will be possible with that
             return "Error, Input missing";
         }
 
-        // handle special operations
+        // handle special operations such as sin and cosine etc.
         if (input.startsWith("s") || input.startsWith("v") || input.startsWith("t") || input.startsWith("s")
                 || input.startsWith("~") || input.startsWith("c") || input.startsWith("|")) {
 
-            return CalculateSpecial(input);
+            return CalculateSpecial(input); // this is another method being called here which actually does the
+                                            // calculation and returns the statement back here
         }
 
         // handle simple operations
-        return CalculateSimple(input);
+        return CalculateSimple(input); // this is also another method being called if the previous "if" statemnt was
+                                       // not used, then this method will return the answer
     }
 
     // Handle special operators based calucations
-    public static String CalculateSpecial(String input) {
-        Scanner moreScan = new Scanner(input);
-        String specialOptr = moreScan.next();
+    public static String CalculateSpecial(String input) { // this method will only be able to return in String Variable
+        Scanner moreScan = new Scanner(input); // the new scanner moreScan just reads first thing of the input from user
+        String specialOptr = moreScan.next(); // next scanner will look for the operator type
 
-        if (specialOptr.length() != 1) {
+        if (specialOptr.length() != 1) { // here it will send an error if the operator is longer than 1 character
             return "Error, Only Single letter Operator";
 
         }
-        if (input.length() == 1) {
+        if (input.length() == 1) { // here it will send out an error if the entire input string is just 1 string
             return "Error, First string is longer than 1";
 
         }
 
-        if (!moreScan.hasNextDouble()) {
+        if (!moreScan.hasNextDouble()) { // it will check if there is another double after the operator variable, if
+                                         // there is no Double type variable after the optr, it will send out an error
             return "Error, Double missing";
         }
 
-        Double y = moreScan.nextDouble();
-        return moreCalc(y, specialOptr.charAt(0));
+        Double y = moreScan.nextDouble(); // here it is just saving the double after the operator in a variable called y
+        return moreCalc(y, specialOptr.charAt(0)); // here it is calling on another method to actually perform the
+                                                   // calculations
 
     }
 
-    public static String CalculateSimple(String input) {
-        Scanner strCalculation = new Scanner(input);
+    public static String CalculateSimple(String input) { // calculation for simpler operations
+        Scanner strCalculation = new Scanner(input); // reads the input
 
-        if (!strCalculation.hasNextDouble()) {
+        if (!strCalculation.hasNextDouble()) { // if there are no doubles , it sends an error
             return "Error: missing double";
         }
         double num1 = strCalculation.nextDouble();
-        if (!strCalculation.hasNext()) {
+        if (!strCalculation.hasNext()) { // if there is no operator after 1st double, it sends an error
             return "Error: missing operator";
         }
         String optr = strCalculation.next();
-        if (optr.length() != 1) {
+        if (optr.length() != 1) { // if the operator length is longer than 1 character then it will send an error
             return "Error: invalid operator: " + optr;
         }
         char optrC = optr.charAt(0);
         if (optrC != '+' && optrC != '-' && optrC != '*' && optrC != '%' && optrC != '/' && optrC != '^'
-                && optrC != '^') {
+                && optrC != '^') { // these are all the symbols for the calculations and if the operator is not one
+                                   // of these it will send an error
             return "Error, Invalid OPerator: " + optrC;
         }
-        if (!strCalculation.hasNextDouble()) {
+        if (!strCalculation.hasNextDouble()) { // if there is no double afteer operator it will send out an error
             return "Error, Invalid 2nd variable";
         }
         double num2 = strCalculation.nextDouble();
-        return Calc(num1, optrC, num2);
+        return Calc(num1, optrC, num2); // this calls upon another method which actually does the calculations
     }
 
-    public static String Calc(double var1, double optr, double var2) {
+    public static String Calc(double var1, double optr, double var2) { // here is the list of all the simple operations
+                                                                       // which calc can do
         if (optr == '%') {
-            return "" + (var1 % var2);
+            return "" + (var1 % var2); // modulo
         } else if (optr == '+') {
-            return "" + (var1 + var2);
+            return "" + (var1 + var2); // addition
         } else if (optr == '/') {
-            return "" + (var1 / var2);
+            return "" + (var1 / var2); // division
         } else if (optr == '*') {
-            return "" + (var1 * var2);
+            return "" + (var1 * var2); // multiplication
         } else if (optr == '^') {
-            return "" + (Math.pow(var1, var2));
+            return "" + (Math.pow(var1, var2)); // power of
         } else if (optr == '-') {
-            return "" + (var1 - var2);
+            return "" + (var1 - var2); // subtraction
         } else {
-            return "Error, Invalid optr: " + optr;
+            return "Error, Invalid optr: " + optr; // if its none of them it will send an error
         }
     }
 
-    public static String moreCalc(double var3, char optr) {
+    public static String moreCalc(double var3, char optr) { // here is the list of all the complicated operations
+                                                            // possible
         if (optr == 'v') {
-            return "" + Math.sqrt(var3);
+            return "" + Math.sqrt(var3); // squareroot of a number
         } else if (optr == 'c') {
-            return "" + Math.cos(var3);
+            return "" + Math.cos(var3); // cosine of a angle degree
         } else if (optr == 't') {
-            return "" + Math.tan(var3);
+            return "" + Math.tan(var3); // tangent of a angle degree
         } else if (optr == 's') {
-            return "" + Math.sin(var3);
+            return "" + Math.sin(var3); // sine of a angle degree
         } else if (optr == '|') {
-            return "" + Math.abs(var3);
+            return "" + Math.abs(var3); // absolute value of a number
         } else if (optr == '~') {
-            return "" + Math.round(var3);
+            return "" + Math.round(var3); // round off to the nearest integer
         } else {
-            return "Error, invalid optr: " + optr;
+            return "Error, invalid optr: " + optr; // if the operation is neither of these all it will send an error
         }
     }
 }
