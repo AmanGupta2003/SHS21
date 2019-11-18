15ebc6f6e087050831403d3004a4f7799a1c4c2c
AmanGupta2003 <57882281+AmanGupta2003@users.noreply.github.com>
2019-11-18 09:43:05 -0800

    Complete

---
+++ Arrays_practice.java
@@ -0,0 +1,14 @@
+//Author: Aman Gupta
+package arrays;
+
+import java.util.*;
+
+public class Arrays_practice {
+    public static void main(Srting[] args) {
+
+        int[] numbers = new int[12];
+        numbers[0] = 1;
+
+        System.out.println("Hello nibba");
+    }
+}
