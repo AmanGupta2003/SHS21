7f6666e42e36faa20cb1063c3ed6de4e302e2882
AmanGupta2003 <57882281+AmanGupta2003@users.noreply.github.com>
2019-11-18 22:51:27 -0800

    commit

--- Arrays_practice.java
+++ Arrays_practice.java
@@ -4,11 +4,12 @@ package arrays;
 import java.util.*;
 
 public class Arrays_practice {
-    public static void main(Srting[] args) {
+    public static void main(String[] args) {
 
         int[] numbers = new int[12];
         numbers[0] = 1;
 
         System.out.println("Hello nibba");
+
     }
 }
