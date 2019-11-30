1e4cd98c351c9e40725496bb675ddd31ea0d4d14
AmanGupta2003 <57882281+AmanGupta2003@users.noreply.github.com>
2019-11-30 11:30:59 -0800

    test

--- Arrays_practice.java
+++ Arrays_practice.java
@@ -6,10 +6,18 @@ import java.util.*;
 public class Arrays_practice {
     public static void main(String[] args) {
 
-        int[] numbers = new int[12];
-        numbers[0] = 1;
+        System.out.println("\n\n");
 
-        System.out.println("Hello nibba");
-
+        int[] numbers = {4, 2, 9, 13, 1};
+        numbers[numbers.length-1] = numbers[0];
+        // so i need to find the length of the array
+        // then i gotta take the firstnumber and 
+        for(int i = 0; i < numbers.length-1; i++ ){
+            numbers[i] = numbers[i + 1];
+            
+        }
+        
+        System.out.println(Arrays.toString(numbers));
+        
     }
 }
