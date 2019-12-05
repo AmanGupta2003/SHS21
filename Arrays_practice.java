f6aac8152a751c984dd462dfb8c1b7e87a728b02
AmanGupta2003 <57882281+AmanGupta2003@users.noreply.github.com>
2019-12-04 18:38:16 -0800

    HW

--- Arrays_practice.java
+++ Arrays_practice.java
@@ -5,102 +5,105 @@ import java.util.*;
 
 public class Arrays_practice {
     public static void main(String[] args) {
-        int[] Dailytemp = new int[7];
-        for (int i = 1; i <= 7; i++) {
-            printDay(Dailytemp, i);
-        }
-        int average = printAverage(Dailytemp);
-        printHigher(Dailytemp, average);
-        PrintArrays(Dailytemp);
-        highest(Dailytemp);
-        lowest(Dailytemp);
-        rotation(Dailytemp);
+        // int[] Dailytemp = new int[7];
+        // for (int i = 1; i <= 7; i++) {
+        //     printDay(Dailytemp, i);
+        // }
+        // int average = printAverage(Dailytemp);
+        // printHigher(Dailytemp, average);
+        // PrintArrays(Dailytemp);
+        // highest(Dailytemp);
+        // lowest(Dailytemp);
+        // rotation(Dailytemp);
+
+
+        
     }
+    
+    // public static void printDay(int[] Dailytemp, int dayNum) {
+    //     System.out.print("Day" + dayNum + "'s high temp: ");
+    //     Scanner temperature = new Scanner(System.in);
+    //     int tempp = temperature.nextInt();
 
-    public static void printDay(int[] Dailytemp, int dayNum) {
-        System.out.print("Day" + dayNum + "'s high temp: ");
-        Scanner temperature = new Scanner(System.in);
-        int tempp = temperature.nextInt();
+    //     Dailytemp[dayNum - 1] = tempp;
+    // }
 
-        Dailytemp[dayNum - 1] = tempp;
-    }
+    // public static int printAverage(int[] Dailytemp) {
 
-    public static int printAverage(int[] Dailytemp) {
+    //     int avg = 0;
+    //     for (int x = 0; x < Dailytemp.length; x++) {
+    //         avg = avg + Dailytemp[x];
+    //     }
+    //     avg = avg / Dailytemp.length;
+    //     System.out.println("Average temp = " + avg);
+    //     return avg;
+    // }
 
-        int avg = 0;
-        for (int x = 0; x < Dailytemp.length; x++) {
-            avg = avg + Dailytemp[x];
-        }
-        avg = avg / Dailytemp.length;
-        System.out.println("Average temp = " + avg);
-        return avg;
-    }
+    // public static void printHigher(int[] Dailytemp, int avg) {
+    //     int AboveAvgCount = 0;
+    //     for (int y = 0; y <= 6; y++) {
+    //         if (Dailytemp[y] > avg) {
+    //             AboveAvgCount++;
+    //         }
+    //     }
+    //     System.out.println(AboveAvgCount + " days were above average");
+    // }
 
-    public static void printHigher(int[] Dailytemp, int avg) {
-        int AboveAvgCount = 0;
-        for (int y = 0; y <= 6; y++) {
-            if (Dailytemp[y] > avg) {
-                AboveAvgCount++;
-            }
-        }
-        System.out.println(AboveAvgCount + " days were above average");
-    }
+    // public static void highest(int[] Dailytemp) {
+    //     int HighNum = 0;
+    //     int highestindex = 0;
+    //     for (int i = 0; i < Dailytemp.length; i++) {
+    //         if (Dailytemp[i] > HighNum) {
+    //             HighNum = Dailytemp[i];
+    //             highestindex = i;
+    //         }
+    //     }
+    //     int ndHighNum = 0;
+    //     for (int x = 0; x < Dailytemp.length; x++) {
+    //         if (x == highestindex) {
+    //             continue;
+    //         }
+    //         if (Dailytemp[x] > ndHighNum) {
+    //             ndHighNum = Dailytemp[x];
+    //         }
+    //     }
+    //     System.out.println("Hottest day = " + HighNum + "," + ndHighNum);
 
-    public static void highest(int[] Dailytemp) {
-        int HighNum = 0;
-        int highestindex = 0;
-        for (int i = 0; i < Dailytemp.length; i++) {
-            if (Dailytemp[i] > HighNum) {
-                HighNum = Dailytemp[i];
-                highestindex = i;
-            }
-        }
-        int ndHighNum = 0;
-        for (int x = 0; x < Dailytemp.length; x++) {
-            if (x == highestindex) {
-                continue;
-            }
-            if (Dailytemp[x] > ndHighNum) {
-                ndHighNum = Dailytemp[x];
-            }
-        }
-        System.out.println("Hottest day = " + HighNum + "," + ndHighNum);
+    // }
 
-    }
+    // public static void lowest(int[] Dailytemp) {
+    //     int coldest = 100;
+    //     int coldestindex = 0;
+    //     int ndcoldest = 100;
+    //     for (int i = 0; i < Dailytemp.length; i++) {
+    //         if (Dailytemp[i] < coldest) {
+    //             coldest = Dailytemp[i];
+    //             coldestindex = i;
+    //         }
 
-    public static void lowest(int[] Dailytemp) {
-        int coldest = 100;
-        int coldestindex = 0;
-        int ndcoldest = 100;
-        for (int i = 0; i < Dailytemp.length; i++) {
-            if (Dailytemp[i] < coldest) {
-                coldest = Dailytemp[i];
-                coldestindex = i;
-            }
+    //     }
+    //     for (int x = 0; x < Dailytemp.length; x++) {
+    //         if (x == coldestindex) {
+    //             continue;
+    //         }
+    //         if (Dailytemp[x] < ndcoldest) {
+    //             ndcoldest = Dailytemp[x];
+    //         }
+    //     }
+    //     System.out.println("Coldest Days: " + coldest + ", " + ndcoldest);
+    // }
 
-        }
-        for (int x = 0; x < Dailytemp.length; x++) {
-            if (x == coldestindex) {
-                continue;
-            }
-            if (Dailytemp[x] < ndcoldest) {
-                ndcoldest = Dailytemp[x];
-            }
-        }
-        System.out.println("Coldest Days: " + coldest + ", " + ndcoldest);
-    }
+    // public static void PrintArrays(int[] Dailytemp) {
+    //     System.out.print("Temperatures: [");
+    //     for (int i = 0; i < Dailytemp.length; i++) {
+    //         System.out.print(Dailytemp[i]);
+    //         if (i < Dailytemp.length - 1) {
+    //             System.out.print(", ");
 
-    public static void PrintArrays(int[] Dailytemp) {
-        System.out.print("Temperatures: [");
-        for (int i = 0; i < Dailytemp.length; i++) {
-            System.out.print(Dailytemp[i]);
-            if (i < Dailytemp.length - 1) {
-                System.out.print(", ");
-
-            }
-        }
-        System.out.println("]");
-    }
+    //         }
+    //     }
+    //     System.out.println("]");
+    // }
     public static void rotation(int[] Dailytemp){
         //System.out.println("Rotated Arrays: ");
         // int original1 = Dailytemp[0];
@@ -113,19 +116,19 @@ public class Arrays_practice {
         //     Dailytemp[i] = Dailytemp[i+1];
         //     System.out.print(Dailytemp[i] + ",");
         // }
-        int lastnumber = Dailytemp[6];
-        for(int x = Dailytemp.length-1; x >= 0; x-- ){
-            if(x == 0){
-                Dailytemp[0] = lastnumber;
+        // int lastnumber = Dailytemp[6];
+        // for(int x = Dailytemp.length-1; x >= 0; x-- ){
+        //     if(x == 0){
+        //         Dailytemp[0] = lastnumber;
                 
-            }else {
-            Dailytemp[x] = Dailytemp[x - 1];
-            }
+        //     }else {
+        //     Dailytemp[x] = Dailytemp[x - 1];
+        //     }
             
-        }
-        System.out.println("Rotation: ");
-        for(int y = 0; y < Dailytemp.length; y++){
-            System.out.print(Dailytemp[y] + " ");
-        }
+        // }
+        // System.out.println("Rotation: ");
+        // for(int y = 0; y < Dailytemp.length; y++){
+        //     System.out.print(Dailytemp[y] + " ");
+        // }
     }
 }
--- Test3.java
+++ Test3.java
@@ -6,18 +6,136 @@ import java.util.*;
 public class Test3 {
     public static void main(final String[] args) {
 
-        String cmd = "";
-        do {
-            Scanner scan = new Scanner(System.in);
-            cmd = scan.nextLine();
-            executeCommand(cmd);
+       
+    }
+// question 1
+    public static int FinalIndex(int [] data, int var){
+        int occ = -1; 
 
-        } while (!cmd.equals("quit"));
-
+        for(int x = 0; x < data.length; x++ ){
+            if{data[x] == var}{
+                occ = x;
+            }
+        }
+        return occ;
     }
 
-    public static void executeCommand(final String cmd)
-    {
-        System.out.println("Executing command: " + cmd);
+// question 2
+    public static int something( int [] data){
+        int var; 
+
+        Arrays.sort(input);
+
+        var = input[data.length] - data[0] + 1; 
+        return var;
     }
+
+// question 3
+    public static int range(int[] data, int y, int z){
+        
+        int whatever;
+        whatever = 0; 
+        
+        for(int x= 0; x < data.length; x++){
+            if( y <= data[x] && data[x] <= z){
+                whatever++;
+            }
+        }
+        
+        
+        return whatever;
+    }
+
+// question 5
+    public static int mode(int[] data){
+        int m = 0; 
+
+        int{} tallyit = new int[100];
+
+        for(int x = 0; x < tallyit.length; x++){
+            if(tallyit[x] > tallyit[m]){
+                m = x; 
+            }
+        }
+        return m;
+    }
+
+// question 8
+    public static double median(int [] data){
+        Arrays.sort(data); 
+
+        if(data.length % 2 != 0){
+            
+            return data[data.length / 2];
+        }else 
+        {
+            return (data); 
+        }
+
+    }
+// question 10
+
+    public static boolean sorted(double [] data){
+        boolean s = true; 
+
+        double lastnumber = -99999999; 
+
+        for(int x = 0; x< data.length; x++){
+            if(lastnumber <= data[x]){
+                lastnumber = data[x]; 
+
+            } else
+                {
+                    s = false; 
+                }
+        }
+        return s; 
+    }
+
+
+    // HOMEWORK FOR DAY 2
+
+// question 11
+
+public static boolen unique(int [] data){
+    boolean u = true; 
+
+    for(int x;){
+        for(int i = x; i < data.length; i++){
+            if(x == data[x]){
+                u = false;
+            }
+        }
+    }
+    return u;
+}
+
+// question 12 sorry i dont understand this question 
+//properly, i dont know how to solve it
+
+// question 13
+
+public static int sequence(int [] data){
+    if(data.length == 0){
+        return 0; 
+    }
+
+    int length = 1; 
+    
+    int length2 = 1;
+    
+    for(int x = 1; x< data.length; x++){
+        if(data[x-1] <= data[x]){
+            length2++; 
+        } else{
+            if(length2 > length){
+                length = length2;
+            }
+            length2 = 1;
+        }
+    }
+    return Math.max(length);
+}
+
+
 }
